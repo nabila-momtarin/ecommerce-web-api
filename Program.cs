246804e5bf8335158cccf607c9cca97c6ecdea1d
nabila-momtarin <142966435+nabila-momtarin@users.noreply.github.com>
2025-09-27 23:52:35 +0600

    commented

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
-
+// api
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
